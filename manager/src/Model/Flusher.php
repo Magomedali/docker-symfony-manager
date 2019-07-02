7e2ca6843282acf4014a479d3cf25621f13e6b03
Magomedali <web-ali@yandex.ru>
2019-07-02 21:53:40 +0300

    implemeented UserRepository

---
+++ manager/src/Model/Flusher.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Model;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+class Flusher {
+
+	private $em;
+
+
+	public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function flush():void
+    {
+        $this->em->flush();
+    }
+    
+}
